f911184c4f3bd8b52c4ebaecd0fdf9ebcf7e34b7
fatemanazari <fatemanazari78@gmail.com>
2023-08-23 01:01:59 +0330

    Create the UI of calculator using component of react

---
+++ src/components/Calculator.js
@@ -0,0 +1,40 @@
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="row">
+        <input className="input-cell" type="text" placeholder="0" />
+      </div>
+      <div className="row">
+        <button className="button-cell" type="button">AC</button>
+        <button className="button-cell" type="button">+/-</button>
+        <button className="button-cell" type="button">%</button>
+        <button className="button-cell last-column" type="button">÷</button>
+      </div>
+      <div className="row">
+        <button className="button-cell" type="button">7</button>
+        <button className="button-cell" type="button">8</button>
+        <button className="button-cell" type="button">9</button>
+        <button className="button-cell last-column" type="button">x</button>
+      </div>
+      <div className="row">
+        <button className="button-cell" type="button">4</button>
+        <button className="button-cell" type="button">5</button>
+        <button className="button-cell" type="button">6</button>
+        <button className="button-cell last-column" type="button">-</button>
+      </div>
+      <div className="row">
+        <button className="button-cell" type="button">1</button>
+        <button className="button-cell" type="button">2</button>
+        <button className="button-cell" type="button">3</button>
+        <button className="button-cell last-column" type="button">+</button>
+      </div>
+      <div className="row">
+        <button className="button-cell zero-cell" type="button">0</button>
+        <button className="button-cell" type="button">.</button>
+        <button className="button-cell last-column" type="button">=</button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
